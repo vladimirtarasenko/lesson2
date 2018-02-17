95ae1c5a3bf604496f36e8a36d65ed8c50c1ac42
Vladimir Tarasenko <vladimirtarasenko33@gmail.com>
2018-02-17 17:48:56 +0300

    Task #2

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,14 @@ public class Main {
 
 
 
-            String s = "Я изучаю Java";
-            System.out.println(s.length());
-            String s1=s.substring(0, s.length()/2);
-            String s2=s.substring(s.length()/2);
-            System.out.println(s1);
-            System.out.println(s2);
+            int x=1027;
+            int y=x%10;
+            if(y==7) {
+                System.out.println("Последняя цифра " + x + " есть семерка");
+            }
+            else {
+                System.out.println("Последняя цифра " + x + " не есть семерка");
+            }
 
         }
     }
