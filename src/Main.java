f706a3616bfd621e43afcd66f4174969f7fa1802
Vladimir Tarasenko <vladimirtarasenko33@gmail.com>
2018-02-17 17:41:56 +0300

    Task #1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+            String s = "Я изучаю Java";
+            System.out.println(s.length());
+            String s1=s.substring(0, s.length()/2);
+            String s2=s.substring(s.length()/2);
+            System.out.println(s1);
+            System.out.println(s2);
+
+        }
+    }
